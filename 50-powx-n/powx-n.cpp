2199da0c7cfecc1d72c8c1dae936832a906a6e7a
Dhruv Gupta <120174191+DDugs@users.noreply.github.com>
2025-02-13 18:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.69%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        double myPow(double x, int n) {
+                //base case 
+                        if(n == 0 ) return 1.0;
+                                if(n == INT_MIN) return 1/myPow(x , -(n + 1) ) * x;
+                                        if(n < 0 ) return 1/myPow(x , -n);
+                                                double half = myPow(x , n/2);
+                                                        if(n % 2 == 0 ) return half * half;
+                                                                else return half * half * x;
+                                                                    }
+                                                                    };
