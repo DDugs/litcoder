677141d4c71c6df1f607f212739519a02c6ca610
Dhruv Gupta <120174191+DDugs@users.noreply.github.com>
2025-02-14 01:11:27 +0530

    Time: 0 ms (100.00%) | Memory: 61.4 MB (7.83%) - LeetSync

---
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+        vector<TreeNode*> nodes;
+            
+                void inorder(TreeNode* root) {
+                        if (!root) return;
+                                inorder(root->left);
+                                        nodes.push_back(root);
+                                                inorder(root->right);
+                                                    }
+                                                        
+                                                            void recoverTree(TreeNode* root) {
+                                                                    inorder(root);
+                                                                            TreeNode *first = nullptr, *second = nullptr;
+                                                                                    
+                                                                                            for (int i = 0; i < nodes.size() - 1; i++) {
+                                                                                                        if (nodes[i]->val > nodes[i + 1]->val) {
+                                                                                                                        if (!first) first = nodes[i];  // First violation
+                                                                                                                                        second = nodes[i + 1];  // Second violation
+                                                                                                                                                    }
+                                                                                                                                                            }
+                                                                                                                                                                    swap(first->val, second->val);  // Swap to fix the BST
+                                                                                                                                                                        }
+                                                                                                                                                                        };
